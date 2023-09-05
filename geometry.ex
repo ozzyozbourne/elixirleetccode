bf75b04703633cf5ca966df1c990ae7f9a3246d1
ozzy ozbourne <khanosaid726@gmail.com>
2023-09-05 05:32:34 +0530

    fib in action

--- geometry.ex
+++ geometry.ex
@@ -72,4 +72,11 @@ defmodule Geometry do
   defmodule Palindrome do 
     def isPalindrome?(value), do: value == String.reverse(value)
   end
+  
+  defmodule Fibonacci do 
+    def fib(0), do: 0
+    def fib(1), do: 1
+    def fib(n), do: fib(n-1)+fib(n-2)
+  end
+
 end
